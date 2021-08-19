95265b5bd8557aa1f515172d090b1ecfaea56ad3
23Shivani <shivani.gharve.2310@gmail.com>
2021-08-20 00:43:32 +0530

    Added: To reterive all data from address book table

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class AddressBook {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Scanner s = new Scanner(System.in);
+		
+		AddressBookRepo repo = new AddressBookRepo();
+		List<Contacts> contact = repo.findAll();
+		contact.forEach(System.out::println);
+	}
+}
